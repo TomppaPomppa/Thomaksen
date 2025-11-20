a60a3683989960a8ec073c09721a365a90ada2b2
Thomas, Vainikainen <ag2982@student.jamk.fi>
2025-11-20 14:29:58 +0200

    Toimii

---
+++ tests/app-test.js
@@ -0,0 +1,10 @@
+import { describe, test, expect, it } from 'vitest';
+const Todo = require('../public/app.js');
+
+describe('Todo testaus', function () {
+  const todo = Todo;
+
+  it('Tarkistetaan', function () {});
+
+  it('Tarkistetaan', function () {});
+});
